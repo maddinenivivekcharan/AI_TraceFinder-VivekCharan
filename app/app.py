d3088df99a3596f87d055865de6880c858a1f584
VivekCharan <maddinenivivekcharan@gmail.com>
2025-10-07 10:41:59 +0530

    Updated

--- app/app.py
+++ app/app.py
@@ -20,6 +20,7 @@ HIT_THR = 0.85
 MIN_HITS = 2
 
 # ---------------- Paths (repo-relative) ----------------
+from pathlib import Path
 BASE_DIR = Path(file).resolve().parent
 ART_SCN = BASE_DIR / "models"
 TAMP_ROOT = ART_SCN / "Tampered images"
